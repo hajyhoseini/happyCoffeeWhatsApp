3f9e5d3547fc199ddfa5d32602026331e85bf79e
hajyhoseini <hajy3843@gmail.com>
2025-02-03 23:11:51 +0330

    4end

--- components/detailical/buyBasket/cartItem.jsx
+++ components/detailical/buyBasket/cartItem.jsx
@@ -1,6 +1,39 @@
+import { useCart } from "@/context/cartContext";
+import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
 
-const CartItem = ({ item, handleIncrease, handleDecrease, removeFromCart, isDarkMode }) => {
+const CartItem = ({ item, isDarkMode }) => {
+  const { cart, setCart, addToCart, removeFromCart } = useCart();
+
+  // افزایش تعداد
+  const handleIncrease = (productName) => {
+    const updatedCart = cart.map((product) => {
+      if (product.name === productName) {
+        return { ...product, quantity: product.quantity + 1 };
+      }
+      return product;
+    });
+    setCart(updatedCart);
+  };
+
+  // کاهش تعداد
+  const handleDecrease = (productName) => {
+    const updatedCart = cart.map((product) => {
+      if (product.name === productName) {
+        // جلوگیری از کاهش تعداد به کمتر از 1
+        return { ...product, quantity: product.quantity > 1 ? product.quantity - 1 : 1 };
+      }
+      return product;
+    });
+    setCart(updatedCart);
+  };
+
+  // حذف محصول از سبد خرید
+  const handleRemove = (productName) => {
+    const updatedCart = cart.filter((product) => product.name !== productName);
+    setCart(updatedCart);
+  };
+
   return (
     <li
       className={`text-base sm:text-lg p-3 sm:p-4 my-2 rounded-md ${
@@ -33,7 +66,7 @@ const CartItem = ({ item, handleIncrease, handleDecrease, removeFromCart, isDark
         variant="danger"
         size="sm"
         className="ml-4"
-        onClick={() => removeFromCart(item.name)}
+        onClick={() => handleRemove(item.name)}
       >
         حذف
       </Button>
